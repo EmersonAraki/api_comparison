53b9bd79059c53cfd4ec97d2412def89116e93a2
Emerson Manabu Araki <emersonmanabuaraki@gmail.com>
2026-06-02 19:41:31 -0300

    fix: add context hash to GraphQL schema execute

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -5,7 +5,12 @@ class GraphqlController < ApplicationController
     variables = prepare_variables(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
-    result = ApiComparisonSchema.execute(query, variables: variables, operation_name: operation_name)
+    result = ApiComparisonSchema.execute(
+      query,
+      variables: variables,
+      context: {},
+      operation_name: operation_name
+    )
     render json: result
   rescue StandardError => e
     raise e unless Rails.env.development?
