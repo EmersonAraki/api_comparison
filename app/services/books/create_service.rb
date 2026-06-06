ec1433ce8de1abcc8714a5d854ebf94c6779b9de
Emerson Manabu Araki <emersonmanabuaraki@gmail.com>
2026-06-06 10:52:58 -0300

    fix: eager load author association after save in CreateService

--- app/services/books/create_service.rb
+++ app/services/books/create_service.rb
@@ -5,7 +5,7 @@ module Books
     def call(title:, published_year:, author_id:)
       book = Book.new(title: title, published_year: published_year, author_id: author_id)
       if book.save
-        Result.new(success: true, record: book, errors: [])
+        Result.new(success: true, record: Book.includes(:author).find(book.id), errors: [])
       else
         Result.new(success: false, record: nil, errors: book.errors.full_messages)
       end
