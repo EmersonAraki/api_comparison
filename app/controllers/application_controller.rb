c291a99b00eedecee1ea51e32eda505f1ac92588
Emerson Manabu Araki <emersonmanabuaraki@gmail.com>
2026-06-06 10:52:50 -0300

    fix: add rescue_from StandardError in ApplicationController for JSON 500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from StandardError, with: :internal_server_error_response
+
+  private
+
+  def internal_server_error_response(err)
+    logger.error err.message
+    render json: { errors: [ "Internal server error" ] }, status: :internal_server_error
+  end
 end
